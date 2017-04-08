98f09b3a1b315d778db5f4ee07fd36e491ad9fbc
Linus Birgerstam <linus@birgerstam.se>
2017-04-08 20:08:28 +0200

    Added BundleOptions

--- src/Bundler/Bundle.cs
+++ src/Bundler/Bundle.cs
@@ -34,102 +34,129 @@ namespace Bundler {
         /// <summary>
         /// Renders the correct html to create a stylesheet link to the bundled css representing the given files.
         /// </summary>
-        /// <param name="fileNames">
-        /// The file names, without the directory path, to link to. These can be .css, .less, .sass, and .scss files or an extension-less token representing an
-        /// external file of the given formats as configured in the web.config.
-        /// </param>
+        /// <param name="fileNames">The files to link to. These can be .css, .less, .sass, and .scss files.</param>
         /// <returns>
         /// The <see cref="HtmlString"/> containing the stylesheet link.
         /// </returns>
         public static HtmlString Styles(params string[] fileNames) {
-            return Styles(null, fileNames);
+            return Styles(GetDefaultBundleOptions(), null, fileNames);
         }
 
         /// <summary>
         /// Renders the correct html to create a stylesheet link to the bundled css representing the given files.
         /// </summary>
-        /// <param name="mediaQuery">
-        /// The media query to apply to the link. For reference see:
-        /// <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Media_queries?redirectlocale=en-US&amp;redirectslug=CSS%2FMedia_queries"/>Media Queries<a/> 
-        /// </param>
-        /// <param name="fileNames">
-        /// The file names, without the directory path, to link to. These can be .css, .less, .sass, and .scss files or an extension-less token representing an
-        /// external file of the given formats as configured in the web.config.
-        /// </param>
+        /// <param name="options">Bundle options deciding how to combine and/or minify the given files.</param>
+        /// <param name="fileNames">The files to link to. These can be .css, .less, .sass, and .scss files.</param>
+        /// <returns>
+        /// The <see cref="HtmlString"/> containing the stylesheet link.
+        /// </returns>
+        public static HtmlString Styles(BundleOptions options, params string[] fileNames) {
+            return Styles(options, null, fileNames);
+        }
+
+        /// <summary>
+        /// Renders the correct html to create a stylesheet link to the bundled css representing the given files.
+        /// </summary>
+        /// <param name="mediaQuery">The media query to apply to the link.</param>
+        /// <param name="fileNames">The files to link to. These can be .css, .less, .sass, and .scss files.</param>
         /// <returns>
         /// The <see cref="HtmlString"/> containing the stylesheet link.
         /// </returns>
         public static HtmlString Styles(HtmlString mediaQuery, params string[] fileNames) {
-            StringBuilder stringBuilder = new StringBuilder();
-            HttpContext context = HttpContext.Current;
+            return Styles(GetDefaultBundleOptions(), mediaQuery, fileNames);
+        }
 
-            // Combine and Minify on release.
-            if (!context.IsDebuggingEnabled) {
-                string fileContent = AsyncHelper.RunSync(() => StyleProcessor.ProcessCssCrunchAsync(context, true, fileNames));
+        /// <summary>
+        /// Renders the correct html to create a stylesheet link to the bundled css representing the given files.
+        /// </summary>
+        /// <param name="options">Bundle options deciding how to combine and/or minify the given files.</param>
+        /// <param name="mediaQuery">The media query to apply to the link.</param>
+        /// <param name="fileNames">The files to link to. These can be .css, .less, .sass, and .scss files.</param>
+        /// <returns>
+        /// The <see cref="HtmlString"/> containing the stylesheet link.
+        /// </returns>
+        public static HtmlString Styles(BundleOptions options, HtmlString mediaQuery, params string[] fileNames) {
+            bool combined = options == BundleOptions.Combined || options == BundleOptions.MinifiedAndCombined;
+            bool minified = options == BundleOptions.Minified || options == BundleOptions.MinifiedAndCombined;
+            string ext = minified ? ".min.css" : ".css";
+
+            HttpContext context = HttpContext.Current;
+            if (combined) {
+                // Combine files
+                string fileContent = AsyncHelper.RunSync(() => StyleProcessor.ProcessCssCrunchAsync(context, minified, fileNames));
                 if (!string.IsNullOrWhiteSpace(fileContent)) {
-                    string fileName = $"{fileContent.ToMd5Fingerprint()}.css";
+                    string fileName = $"{fileContent.ToMd5Fingerprint()}{ext}";
                     return new HtmlString(string.Format(CssPhysicalFileTemplate,
                         AsyncHelper.RunSync(() => ResourceHelper.CreateResourcePhysicalFileAsync(fileName, fileContent)),
                         mediaQuery));
                 }
-            }
+            } else {
+                // Render files separately
+                StringBuilder stringBuilder = new StringBuilder();
 
-            // Expand .bundle files
-            fileNames = ResourceHelper.ExpandBundles(context, false, null, fileNames);
+                // Expand .bundle files
+                fileNames = ResourceHelper.ExpandBundles(context, false, null, fileNames);
 
-            // Render files separately and unminified for debug mode.
-            foreach (string name in fileNames) {
-                string fileContent = AsyncHelper.RunSync(() => StyleProcessor.ProcessCssCrunchAsync(context, false, name));
-                if (!string.IsNullOrWhiteSpace(fileContent)) {
-                    string fileName = $"{Path.GetFileNameWithoutExtension(name)}.{fileContent.ToMd5Fingerprint()}.css";
-                    stringBuilder.AppendFormat(CssPhysicalFileTemplate,
-                        AsyncHelper.RunSync(() => ResourceHelper.CreateResourcePhysicalFileAsync(fileName, fileContent)),
-                        mediaQuery);
-                    stringBuilder.AppendLine();
+                foreach (string name in fileNames) {
+                    string fileContent = AsyncHelper.RunSync(() => StyleProcessor.ProcessCssCrunchAsync(context, minified, name));
+                    if (!string.IsNullOrWhiteSpace(fileContent)) {
+                        string fileName = $"{Path.GetFileNameWithoutExtension(name)}.{fileContent.ToMd5Fingerprint()}{ext}";
+                        stringBuilder.AppendFormat(CssPhysicalFileTemplate,
+                            AsyncHelper.RunSync(() => ResourceHelper.CreateResourcePhysicalFileAsync(fileName, fileContent)),
+                            mediaQuery);
+                        stringBuilder.AppendLine();
+                    }
                 }
+                return new HtmlString(stringBuilder.ToString());
             }
 
-            return new HtmlString(stringBuilder.ToString());
+            return null;
         }
 
         /// <summary>
         /// Renders the correct html to create a script tag linking to the bundled JavaScript representing the given files.
         /// </summary>
-        /// <param name="fileNames">
-        /// The file names, without the directory path, to link to. These can be .js, and .coffee files or an extension-less token representing an
-        /// external file of the given formats as configured in the web.config.
-        /// </param>
+        /// <param name="fileNames">The .js files to link to.</param>
         /// <returns>
         /// The <see cref="HtmlString"/> containing the script tag with the correct link.
         /// </returns>
         public static HtmlString Scripts(params string[] fileNames) {
-            return Scripts(JavaScriptLoadBehaviour.Inline, fileNames);
+            return Scripts(GetDefaultBundleOptions(), JavaScriptLoadBehaviour.Inline, fileNames);
         }
 
         /// <summary>
         /// Renders the correct html to create a script tag linking to the bundled JavaScript representing the given files.
         /// </summary>
-        /// <param name="behaviour">
-        /// The <see cref="JavaScriptLoadBehaviour"/> describing the way the browser should load the JavaScript into the page.
-        /// </param>
-        /// <param name="fileNames">
-        /// The file names, without the directory path, to link to. These can be .js, and .coffee files or an extension-less token representing an
-        /// external file of the given formats as configured in the web.config.
-        /// </param>
+        /// <param name="options">Bundle options deciding how to combine and/or minify the given files.</param>
+        /// <param name="fileNames">The .js files to link to.</param>
         /// <returns>
         /// The <see cref="HtmlString"/> containing the script tag with the correct link.
         /// </returns>
-        public static HtmlString Scripts(JavaScriptLoadBehaviour behaviour, params string[] fileNames) {
-            StringBuilder stringBuilder = new StringBuilder();
+        public static HtmlString Scripts(BundleOptions options, params string[] fileNames) {
+            return Scripts(options, JavaScriptLoadBehaviour.Inline, fileNames);
+        }
+
+        /// <summary>
+        /// Renders the correct html to create a script tag linking to the bundled JavaScript representing the given files.
+        /// </summary>
+        /// <param name="options">Bundle options deciding how to combine and/or minify the given files.</param>
+        /// <param name="behaviour">The <see cref="JavaScriptLoadBehaviour"/> describing the way the browser should load the JavaScript into the page.</param>
+        /// <param name="fileNames">The .js files to link to.</param>
+        /// <returns>
+        /// The <see cref="HtmlString"/> containing the script tag with the correct link.
+        /// </returns>
+        public static HtmlString Scripts(BundleOptions options, JavaScriptLoadBehaviour behaviour, params string[] fileNames) {
+            bool combined = options == BundleOptions.Combined || options == BundleOptions.MinifiedAndCombined;
+            bool minified = options == BundleOptions.Minified || options == BundleOptions.MinifiedAndCombined;
+            string ext = minified ? ".min.js" : ".js";
+
             HttpContext context = HttpContext.Current;
-
             string behaviourParam = behaviour == JavaScriptLoadBehaviour.Inline ? string.Empty : " " + behaviour.ToString().ToLowerInvariant();
-
-            // Minify on release.
-            if (!context.IsDebuggingEnabled) {
-                string fileContent = AsyncHelper.RunSync(() => ScriptProcessor.ProcessJavascriptCrunchAsync(context, true, fileNames));
+            if (combined) {
+                // Combine files
+                string fileContent = AsyncHelper.RunSync(() => ScriptProcessor.ProcessJavascriptCrunchAsync(context, minified, fileNames));
                 if (!string.IsNullOrWhiteSpace(fileContent)) {
-                    string fileName = $"{fileContent.ToMd5Fingerprint()}.js";
+                    string fileName = $"{fileContent.ToMd5Fingerprint()}{ext}";
                     return
                         new HtmlString(
                             string.Format(
@@ -138,25 +165,36 @@ namespace Bundler {
                                     () => ResourceHelper.CreateResourcePhysicalFileAsync(fileName, fileContent)),
                                 behaviourParam));
                 }
-            }
+            } else {
+                // Render files separately
+                StringBuilder stringBuilder = new StringBuilder();
 
-            // Expand .bundle files
-            fileNames = ResourceHelper.ExpandBundles(context, false, null, fileNames);
+                // Expand .bundle files
+                fileNames = ResourceHelper.ExpandBundles(context, false, null, fileNames);
 
-            // Render them separately and unminified for debug mode.
-            foreach (string name in fileNames) {
-                string fileContent = AsyncHelper.RunSync(() => ScriptProcessor.ProcessJavascriptCrunchAsync(context, false, name));
-                if (!string.IsNullOrWhiteSpace(fileContent)) {
-                    string fileName = $"{Path.GetFileNameWithoutExtension(name)}.{fileContent.ToMd5Fingerprint()}.js";
-                    stringBuilder.AppendFormat(
-                        JavaScriptPhysicalFileTemplate,
-                        AsyncHelper.RunSync(() => ResourceHelper.CreateResourcePhysicalFileAsync(fileName, fileContent)),
-                        behaviourParam);
-                    stringBuilder.AppendLine();
+                foreach (string name in fileNames) {
+                    string fileContent = AsyncHelper.RunSync(() => ScriptProcessor.ProcessJavascriptCrunchAsync(context, false, name));
+                    if (!string.IsNullOrWhiteSpace(fileContent)) {
+                        string fileName = $"{Path.GetFileNameWithoutExtension(name)}.{fileContent.ToMd5Fingerprint()}{ext}";
+                        stringBuilder.AppendFormat(
+                            JavaScriptPhysicalFileTemplate,
+                            AsyncHelper.RunSync(() => ResourceHelper.CreateResourcePhysicalFileAsync(fileName, fileContent)),
+                            behaviourParam);
+                        stringBuilder.AppendLine();
+                    }
                 }
+                return new HtmlString(stringBuilder.ToString());
             }
 
-            return new HtmlString(stringBuilder.ToString());
+            return null;
+        }
+
+        /// <summary>
+        /// If debugging is enabled in web.config, renders individual unminified files, otherwise combines and minifies files into a single file.
+        /// </summary>
+        /// <returns></returns>
+        private static BundleOptions GetDefaultBundleOptions() {
+            return HttpContext.Current.IsDebuggingEnabled ? BundleOptions.Normal : BundleOptions.MinifiedAndCombined;
         }
     }
 }
