a1dc7fb757ae32f1b6caf671a5b3e2a181201af9
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2023-01-27 19:26:52 +0100

    Fixing errors and update to AsyncKeyedLock 6.1.1

--- src/Website/Extensions/BundlerExtensions.cs
+++ src/Website/Extensions/BundlerExtensions.cs
@@ -1,12 +1,5 @@
-﻿using Bundler.Extensions;
-using Bundler.Helpers;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Website.Extensions {
+﻿namespace Website.Extensions
+{
 
     /// <summary>
     /// Extension methods for rendering CSS and JavaScript links onto a webpage.
